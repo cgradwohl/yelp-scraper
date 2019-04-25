11808af8982e87fd460f288ea424c462827a703c
Chris Gradwohl <christophergradwohl@gmail.com>
2019-04-25 08:57:19 -0700

    parsePage func

--- utils/parsePage.js
+++ utils/parsePage.js
@@ -1,1 +1,24 @@
-module.exports = () => {};
+const cheerio = require('cheerio');
+
+module.exports = (page) => {
+  try {
+    const $ = cheerio.load(page);
+    const rating = $('.rating-info .i-stars')
+      .attr('title')
+      .trim()
+      .split(' ')[0];
+    const reviewCount = $('.rating-info .review-count')
+      .text()
+      .trim()
+      .split(' ')[0];
+
+    const data = {
+      rating,
+      reviewCount
+    };
+    return Promise.resolve(data);
+  } catch (error) {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject(`Error parsing page: ${JSON.stringify(error)}`);
+  }
+};
