c57a254d9c20e810b529b21a4d47b39c44085b1b
Chris Gradwohl <christophergradwohl@gmail.com>
2019-04-25 08:57:27 -0700

    save db func

--- utils/saveRatingsToDB.js
+++ utils/saveRatingsToDB.js
@@ -1,1 +1,28 @@
-module.exports = () => {};
+const uuid = require('uuid');
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (data, businessName) => {
+  const { rating, reviewCount } = data;
+  const ts = JSON.stringify(new Date());
+  const id = uuid.v1();
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id,
+      businessName,
+      reviewCount,
+      rating,
+      ts
+    }
+  };
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      return new Error(`Error Saving Data to DB ${JSON.stringify(error)}`);
+    }
+    return params.Item;
+  });
+};
